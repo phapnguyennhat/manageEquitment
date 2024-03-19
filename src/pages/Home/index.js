a46512fe61274d1990a050c2c762240f9b388739
HoangLinhDo <linh.dohoang@hcmut.edu.vn>
2024-03-19 20:15:35 +0700

    test

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,36 +5,9 @@ const cx = classNames.bind(styles);
 
 function Home() {
   return (
-    <div className="home">
-      <div className={cx("container-img")}>
-        <div className="home1">
-          <img
-            className={cx("home__img")}
-            alt=""
-            src="https://lms.hcmut.edu.vn/pluginfile.php/3/theme_academi/slide3image/1710760830/sanbong.jpeg"
-          ></img>
-        </div>
-        <div className="home2">
-          <img
-            className={cx("home__img")}
-            alt=""
-            src="https://lms.hcmut.edu.vn/pluginfile.php/3/theme_academi/slide1image/1710760830/slbk.jpg"
-          ></img>
-          <img
-            className={cx("home__img")}
-            alt=""
-            src="https://th.bing.com/th/id/OIP.zLkWIO82DNql2E1FqhEiJwHaHT?rs=1&pid=ImgDetMain"
-          ></img>
-          <img
-            className={cx("home__img")}
-            alt=""
-            src="https://lms.hcmut.edu.vn/pluginfile.php/3/theme_academi/slide2image/1710760830/slbktv.jpg"
-          ></img>
-        </div>
-        <div className="home3">
-          <img alt="" src="" className={cx("home__img")}></img>
-        </div>
-      </div>
+    <div className={cx('homeWrapper')}>
+      <img className={cx('image')} alt='' src='https://th.bing.com/th/id/R.7f6e8dcf36848ca41f6835b55e696765?rik=yVhTKS6%2b%2bOe7KQ&pid=ImgRaw&r=0'></img>
+      <div className={cx('webName')}></div>
     </div>
   );
 }
