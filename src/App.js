073312c5ba5751546ad964323a4a5d5704ef7ca5
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-03-19 11:28:58 +0700

    Tổ chức lại file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-// import logo from './logo.svg';
-import './App.css';
-import Heading from './Heading.js'
-import Home from './Home.js'
-import Footer from './Footer.js'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { publicRoutes } from "./routes";
+import DefaultLayout from "./components/Layout/DefaultLayout";
 
 function App() {
   return (
-    <>
-      <Heading />
-      <Home />
-    <div className='content-wrapper'>
-    
-    </div>
-      <Footer />
-    </>
+    // <>
+    //   <Heading />
+    //   <Home />
+    //   {/* <div className="content-wrapper"></div> */}
+    //   <Footer />
+    // </>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            let Layout = DefaultLayout;
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
