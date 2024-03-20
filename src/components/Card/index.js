863669f77cd4b7f1fc7dc4e98392681e83dcfcca
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-03-20 11:24:39 +0700

    gan xong stock

---
+++ src/components/Card/index.js
@@ -0,0 +1,39 @@
+import classNames from "classnames/bind";
+import styles from "./Card.module.scss";
+import { useState } from "react";
+
+const cx = classNames.bind(styles);
+
+function Card({ src, name, quantity }) {
+  const [soluong, setSoluong] = useState(quantity);
+
+  let msg = `Còn ${soluong}`;
+  if (soluong === 0) {
+    msg = "Hết";
+  }
+
+  const handleAdd = () => {
+    // setSoluong((prev) => prev - getNum);
+  };
+
+  return (
+    <div className={cx("card")}>
+      <div className={cx("card-info")}>
+        <img src={src} alt="not found" className={cx("card-info-img")} />
+        <h3 className={cx("card-info-name")}>{name}</h3>
+        <div className={cx("card-info-msg")}>{msg}</div>
+        <input
+          type="number"
+          min="0"
+          max={quantity}
+          className={cx("card-info-num")}
+        />
+        <button className={cx("card-info-btn")} onClick={handleAdd}>
+          Thêm vào giỏ hàng
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
