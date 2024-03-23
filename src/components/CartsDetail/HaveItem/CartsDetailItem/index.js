53939a1ddcad7a233fb3dd37da7d30d91ef96eaa
Nguyen Nhat Phap <nguyennhatphapbp@gmail.com>
2024-03-23 14:45:48 +0700

    gi do

--- src/components/CartsDetail/HaveItem/CartsDetailItem/index.js
+++ src/components/CartsDetail/HaveItem/CartsDetailItem/index.js
@@ -6,8 +6,10 @@ const cx = classNames.bind(styles);
 function CartsDetailItem({ src, name, getSL }) {
   return (
     <div className={cx("cart-item")}>
-      <img src={src} alt="not found" className={cx("cart-item-img")} />
-      <span className={cx("cart-item-name")}>{name}</span>
+      <div className={cx("wrap-img")}>
+        <img src={src} alt="not found" className={cx("cart-item-img")} />
+        <span className={cx("cart-item-name")}>{name}</span>
+      </div>
       <div className={cx("wrap-quantity")}>
         <span className={cx("cart-item-multi")}>x</span>
         <span className={cx("cart-item-quantity")}>{getSL}</span>
